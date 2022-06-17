8736084a7ba194f3ba8b1717c50864e2bb5f3c27
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-18 02:56:03 +0600

    project done

---
+++ README.md
@@ -0,0 +1,1 @@
+# html-table-xls-pdf
---
+++ assets/js/scripts.js
@@ -0,0 +1,85 @@
+(function($) {
+    "use strict";
+
+
+    $(function () {
+        for (var nk = window.location, o = $(".nano-content li a").filter(function () {
+            return this.href == nk;
+        })
+            .addClass("active")
+            .parent()
+            .addClass("active"); ;) {
+            if (!o.is("li")) break;
+            o = o.parent()
+                .addClass("d-block")
+                .parent()
+                .addClass("active");
+        }
+    });
+
+
+    /* 
+    ------------------------------------------------
+    Sidebar open close animated humberger icon
+    ------------------------------------------------*/
+
+    $(".hamburger").on('click', function() {
+        $(this).toggleClass("is-active");
+    });
+
+
+
+
+
+    /* TO DO LIST 
+    --------------------*/
+    $(".tdl-new").on('keypress', function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 13) {
+            var v = $(this).val();
+            var s = v.replace(/ +?/g, '');
+            if (s == "") {
+                return false;
+            } else {
+                $(".tdl-content ul").append("<li><label><input type='checkbox'><i></i><span>" + v + "</span><a href='#' class='ti-close'></a></label></li>");
+                $(this).val("");
+            }
+        }
+    });
+
+
+    $(".tdl-content a").on("click", function() {
+        var _li = $(this).parent().parent("li");
+        _li.addClass("remove").stop().delay(100).slideUp("fast", function() {
+            _li.remove();
+        });
+        return false;
+    });
+
+    // for dynamically created a tags
+    $(".tdl-content").on('click', "a", function() {
+        var _li = $(this).parent().parent("li");
+        _li.addClass("remove").stop().delay(100).slideUp("fast", function() {
+            _li.remove();
+        });
+        return false;
+    });
+
+
+    
+
+
+
+})(jQuery);
+
+
+//Create Order Calculation
+
+function qtyCal()
+{
+    const orderQty = document.getElementById("orderQty").value;
+    const orderExtra = document.getElementById("orderExtra").value =
+        (parseInt(orderQty) / 100) * 5;
+    const Equation = parseInt(orderQty) + orderExtra;
+    document.getElementById("totalQty").value = Equation;
+}
